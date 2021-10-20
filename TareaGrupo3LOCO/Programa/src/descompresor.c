5b46f87cde9c486afe51ea70a7b617f1e0cfae3b
alexis.baladon <alexis.baladon@fing.edu.uy>
2021-10-20 15:47:10 -0300

    Makefile, cola_t y descompresor casi pronto

--- TareaGrupo3LOCO/Programa/src/descompresor.c
+++ TareaGrupo3LOCO/Programa/src/descompresor.c
@@ -5,14 +5,14 @@
 
 #include <stdio.h>
 
-void leerCabezal(int *s,modalidad_t * modalidad); //FUNCION AUXILIAR DESCOMPRESOR
-void inicializarContextos(); //COMPARTIDO
+void leerCabezal(FILE * compressedFile, int *s,modalidad_t * modalidad); //FUNCION AUXILIAR DESCOMPRESOR
+void inicializarExtractos(); //COMPARTIDO
 void inicializarBuffer(); //PIXELIO
 unsigned char obtenerUltimoCaracter(); //PIXELIO
-void determinarContexto(); //PIXELIO
-int comienzaModoDeRun(); //PIXELIO
-void determinarParametroGolombK(); //COMPARTIDO
-void determinarMapeoRice(); //COMPARTIDO
+void determinarContexto(unsigned char * a, unsigned char * b, unsigned char * c, unsigned char * d); //PIXELIO
+unsigned char predecirX(unsigned char a, unsigned char b, unsigned char c); //COMPARTIDO
+int determinarExtracto(unsigned char x, unsigned char a, unsigned char b, unsigned char c); //PIXELIO
+int determinarParametroGolombK(); //COMPARTIDO
 void determinarCodificacionGolomb(); //COMPARTIDO
 
 void descomprimir( char* archivoEntrada, char* archivoSalida ) {
@@ -21,22 +21,28 @@ void descomprimir( char* archivoEntrada, char* archivoSalida ) {
     int s;
     modalidad_t modalidad; 
     int k;
+    int ultimoCaracterLeido;    /* Promoción temporal de x a entero */
     unsigned char x;
+    unsigned char xPrediccion;
+    int fExtracto;              /* f(C) */
+    unsigned char a,b,c,d;      /* Contexto */
+    int output;                 /* Salida del programa */
 
-    leerCabezal(&s,&modalidad); //Lee el cabezal que contiene s y modalidad
     compressedFile = fopen(archivoEntrada,"rb");
     decompressedFile = fopen(archivoSalida,"wb");
-    
-    inicializarContextos();
+
+    leerCabezal(compressedFile,&s,&modalidad);    /* Obtiene del cabezal s y modalidad */
+    inicializarExtractos();
     inicializarBuffer();
     if(modalidad == Run) {
-        
-        while(x = obtenerUltimoCaracter() != EOF) {
-            determinarContexto();
-            if(!comienzaModoDeRun()) {
-                determinarParametroGolombK();
-                determinarMapeoRice();
-                determinarCodificacionGolomb();
+        while(ultimoCaracterLeido = obtenerUltimoCaracter() != EOF) {
+            x = (unsigned char)ultimoCaracterLeido;
+            determinarContexto(&a,&b,&c,&d);
+            xPrediccion = predecirX(a,b,c);
+            fExtracto = determinarExtracto(xPrediccion,a,b,c);
+            if(a != b || b!=c || c==d) {    /* No es modo de run */
+                k = determinarParametroGolombK(fExtracto); //Crear la maldita estructuraaa
+                determinarCodificacionGolomb(k,x-xPrediccion,fExtracto); //METER ESTA FUNCION Y LA DE ABAJO FUERA DEL IF, NECESITO FUNCION EXTRA DE MAPEO DE RICE?
                 actualizarContextos();
             }
             else {
@@ -44,20 +50,20 @@ void descomprimir( char* archivoEntrada, char* archivoSalida ) {
                 do {
                     xAnterior = x;
                     k = 3;
-                    determinarMapeoRice();
                     determinarCodificacionGolomb();
                     actualizarContextos();
-                    x =  obtenerUltimoCaracter();
-                } while(x == xAnterior && x != EOF);
+                    ultimoCaracterLeido =  obtenerUltimoCaracter();
+                    x = (unsigned char)ultimoCaracterLeido;
+                } while(ultimoCaracterLeido != EOF && x == xAnterior);
                 if(x == EOF) break;
             }
         }
     }
     else {
-        while(x = obtenerUltimoCaracter() != EOF) {
-            determinarContexto();
+        while(ultimoCaracterLeido = obtenerUltimoCaracter() != EOF) {
+            x = (unsigned char)ultimoCaracterLeido;
+            fExtracto = determinarContexto(x);
             determinarParametroGolombK();
-            determinarMapeoRice();
             determinarCodificacionGolomb();
             actualizarContextos();
         }
@@ -67,8 +73,12 @@ void descomprimir( char* archivoEntrada, char* archivoSalida ) {
 /*
  * Función auxiliar encargada de obtener los parámetros 's' y
  * la modalidad (Run o Normal) enviados del lado del compresor
+ * 
+ * Precondición:
+ *     -compressedFile ya ha sido inicializado con el archivo
+ *     correspondiente.
  */
-void leerCabezal(int *s,modalidad_t * modalidad) {
+void leerCabezal(FILE * compressedFile, int *s,modalidad_t * modalidad) {
     //PROVISORIO
     *s = 0;
     *modalidad = Normal;
--- TareaGrupo3LOCO/Programa/src/pixelio.c
+++ TareaGrupo3LOCO/Programa/src/pixelio.c
@@ -1,1 +1,45 @@
 #include "../include/pixelio.h"
+
+//Estructura de arreglo circular que hice para otra materia
+#define MAX_BUFFER 123456 //PROVISORIO, este valor va a ser dinámico
+
+typedef struct {
+    int cant;
+    int ini,fin;
+    unsigned char * valores;
+} cola_t;
+
+cola_t crearCola(int size) {
+    cola_t res = {0,0,0,malloc(size*sizeof(unsigned char))}; 
+    return res;
+}
+
+void encolar(cola_t * cola,char elem) {
+    if(cola->cant != 0) {
+        if(cola->ini != 0) {
+            cola->ini--;
+        }
+        else {
+            cola->ini = MAX_BUFFER - 1;
+        }
+    }
+    cola->valores[cola->ini] = elem;
+    cola->cant++;
+}
+
+void destruirCola(cola_t * cola) {
+    free(cola->valores);
+}
+
+/*
+void desencolar(cola_t * cola) {
+        cola->cant--;
+        if(cola->cant != 0) {
+            if(cola->fin != 0) {
+                cola->fin--;
+            }
+            else {
+                cola->fin = MAX_BUFFER - 1;
+            }
+        }
+}*/
