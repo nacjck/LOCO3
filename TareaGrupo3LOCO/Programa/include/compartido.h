5e1e483d6f59154b3c9de956cb97c1dd2d9ddb4e
Ignacio F. Desantis <nacjck@gmail.com>
2021-10-26 19:40:59 -0300

    Función de predicción terminada

--- TareaGrupo3LOCO/Programa/include/compartido.h
+++ TareaGrupo3LOCO/Programa/include/compartido.h
@@ -1,12 +1,14 @@
-/* 
+/*
  * compartido.h
  *
  * Aquí se hallan funciones y estructuras utilizadas
  * tanto en el compresor como en el descompresor
- * 
+ *
  */
 
 #ifndef compartido_h
 #define compartido_h
 
 #endif
+
+typedef unsigned char PIX; // Alias para valores de pixel
--- TareaGrupo3LOCO/Programa/src/compartido.c
+++ TareaGrupo3LOCO/Programa/src/compartido.c
@@ -1,1 +1,40 @@
 #include "../include/compartido.h"
+
+#include <stdio.h> // Para debugg
+
+/* Calcula la predicción del pixel dado su contexto */
+PIX predict(PIX a, PIX b, PIX c) {
+
+  PIX max_ab;
+  PIX min_ab;
+
+  // Máximo y mínimo entre a y b
+  if (a >= b) {
+    max_ab = a;
+    min_ab = b;
+  }
+  else {
+    max_ab = b;
+    min_ab = a;
+  }
+
+  // Fórmula para hat_x
+  PIX hat_x;
+  if (c >= max_ab) {
+    hat_x = min_ab;
+  }
+  else if (c <= min_ab) {
+    hat_x = max_ab;
+  }
+  else {
+    hat_x = a + b -c;
+  }
+
+  return hat_x;
+
+}
+
+// Para test y debugg
+int main() {
+  printf("hat_x=%d", predict(0,11,5));
+}
