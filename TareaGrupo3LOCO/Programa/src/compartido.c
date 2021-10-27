6fd293c0e40d4d57b985ab701f7f43495a9a1209
Ignacio F. Desantis <nacjck@gmail.com>
2021-10-26 22:14:48 -0300

    Extracto terminada.

--- TareaGrupo3LOCO/Programa/src/compartido.c
+++ TareaGrupo3LOCO/Programa/src/compartido.c
@@ -57,11 +57,23 @@ unsigned short n_act(PIX a, PIX b, PIX c, PIX x) {
   return X;
 }
 
+unsigned short extract(unsigned short X, BYTE T, BYTE s) {
+  // Devuelve el extracto f(C)
+  // Máximo s+3 bits
+
+  unsigned short Q, fC;
+
+  Q = (X >> (10-s)); // Cuantización de X
+  fC = ((Q<<3) + T);
+
+  return fC;
+}
+
 // Para test y debugg
 int main() {
-  unsigned int a, b, c, hat_x;
+  unsigned int a, b, c, hat_x, s;
   BYTE T;
-  unsigned short X;
+  unsigned short X, fC;
 
   printf("Ingresar valores de contexto: a, b, c:\n");
   scanf("%u%u%u", &a, &b, &c);
@@ -71,7 +83,12 @@ int main() {
   T = textura(a, b, c, hat_x);
   X = n_act(a, b, c, hat_x);
 
+  printf("Ingresar parámetro s:\n");
+  scanf("%u", &s);
+  fC = extract(X, T, s);
+
   printf("Prediccion: hat_x=%d\n", hat_x);
   printf("Textura: T=%x\n", T);
   printf("Nivel de actividad: X=%u\n", X);
+  printf("Extracto: f(C)=%u", fC);
 }
