cbcb2c240d726017d0fabadaef5b5272ad62b614
Ignacio F. Desantis <nacjck@gmail.com>
2021-11-10 01:08:29 -0300

    Funciones para codificación. Faltan pruebas.
    Rebase de branch 'descompresor' al head del master.

--- TareaGrupo3LOCO/Programa/src/compartido.c
+++ TareaGrupo3LOCO/Programa/src/compartido.c
@@ -1,6 +1,5 @@
 #include "../include/compartido.h"
 
-<<<<<<< HEAD
 #include <stdio.h>
 
 #define R 128
@@ -158,79 +157,6 @@ void liberarExtractos() {
         free(extractos[i]);
     }
     free(extractos);
-=======
-#include <stdio.h> // Para debugg
-
-PIX predict(PIX a, PIX b, PIX c) {
-  /* Calcula la predicción del pixel dado su contexto */
-  PIX max_ab;
-  PIX min_ab;
-
-  // Máximo y mínimo entre a y b
-  if (a >= b) {
-    max_ab = a;
-    min_ab = b;
-  }
-  else {
-    max_ab = b;
-    min_ab = a;
-  }
-
-  // Fórmula para hat_x
-  PIX hat_x;
-  if (c >= max_ab) {
-    hat_x = min_ab;
-  }
-  else if (c <= min_ab) {
-    hat_x = max_ab;
-  }
-  else {
-    hat_x = a + b -c;
-  }
-
-  return hat_x;
-}
-
-
-BYTE textura(PIX a, PIX b, PIX c, PIX x) {
-  // Devuelve el número T con los bits de textura
-  BYTE T;
-
-  T = (c>x) << 2; // MSB
-  T += (b>x) << 1;
-  T += (a>x); // LSB
-
-  return T;
-}
-
-// Nota: La textura y el nivel de actividad se pueden calcular al mismo tiempo
-
-unsigned short n_act(PIX a, PIX b, PIX c, PIX x) {
-  // Devuelve el nivel de actividad X
-  unsigned short X = 0; // Máximo 10 bits
-
-  X += (c >= x) ? (c-x) : (x-c);
-  X += (b >= x) ? (b-x) : (x-b);
-  X += (a >= x) ? (a-x) : (x-a);
-
-  return X;
-}
-
-unsigned short extract(unsigned short X, BYTE T, BYTE s) {
-  // Devuelve el extracto f(C)
-  // Máximo s+3 bits
-
-  unsigned short Q, fC;
-
-  Q = (X >> (10-s)); // Cuantización de X
-  fC = ((Q<<3) + T);
-
-  return fC;
-}
-
-// Para test y debugg
-<<<<<<< HEAD
-=======
 int main() {
   unsigned int a, b, c, hat_x, s;
   BYTE T;
