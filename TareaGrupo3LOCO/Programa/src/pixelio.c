c073309b790eb288588324fdfbb00faa30b02ab9
Ignacio F. Desantis <nacjck@gmail.com>
2021-11-16 00:56:17 -0300

    Corregidos errores de sintaxis.

--- TareaGrupo3LOCO/Programa/src/pixelio.c
+++ TareaGrupo3LOCO/Programa/src/pixelio.c
@@ -2,3 +2,18 @@
 #include <stdio.h>
 
 #include "../include/pixelio.h"
+
+void write_code(FILE* outFile, unsigned int code, uint16_t l, BYTE &buffer, BYTE &buffBits) {
+  // Escribe el siguiente código en el archivo de la imagen comprimida
+
+  BYTE b;
+
+  if (buffBits == 0) { // Si no hay nada en el buffer
+
+    buffBits = l % 8; // Número de bits que sobran
+    buffer = code & ((1<<buffBits) - 1); // LSBs
+
+    // Escribe archivo
+  }
+
+}
