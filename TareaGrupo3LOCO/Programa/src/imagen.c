b5e2e0cedb22075a2ec5a2c71d4dda88fecfd715
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-12-28 14:07:35 -0300

    Error en impresion de P5

--- TareaGrupo3LOCO/Programa/src/imagen.c
+++ TareaGrupo3LOCO/Programa/src/imagen.c
@@ -39,6 +39,7 @@ DatosCabezal escribirCabezalPGM( FILE * archivoInput, FILE * archivoOutput ) {
         puts("Tipo de imagen no soportado.");
         exit(1);
     }
+    fprintf(archivoInput, "%s", tipo);
 
     do {
         c = getc(archivoInput);
