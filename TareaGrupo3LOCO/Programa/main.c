3169a6b4f8c79e1c1304ea221509851eed8e13f4
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2022-02-02 14:36:05 -0300

    Aclaración en main de que -d descomprime y -c comprime

--- TareaGrupo3LOCO/Programa/main.c
+++ TareaGrupo3LOCO/Programa/main.c
@@ -10,14 +10,14 @@
  * main.c
  *
  * Para correr el archivo 'main.c' desde la consola, usar:
- * ./main -d inputFile outputFile
- * ./main -c [modalidad] [-s valorS] [-m] inputFile outputFile
+ * ./main -d inputFile outputFile (al descomprimir)
+ * ./main -c [modalidad] [-s valorS] [-m] inputFile outputFile (al comprimir)
  * 
  * Donde:
  * -c <number> habilita la compresión,
  * si recibe 1 comprime en modo de run,
  * si recibe un 0 o nada lo hace en modo normal.
- * -d habilita la compresión
+ * -d habilita la descompresión
  * -s <number>: valor del parámetro s
  * si no hay parámetro se tomara el valor DEFAULT_S
  * -m silencia el output de la consola (excepto para errores)
